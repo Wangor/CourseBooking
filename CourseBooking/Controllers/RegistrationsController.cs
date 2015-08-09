63b83073dfe33d995245d1f55222be74bae55369
Mattb <info@matt-b.ch>
2015-08-09 14:20:52 +0200

    debug 11

---
+++ CourseBooking/Controllers/RegistrationsController.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RegistrationsController.cs" company="Bärtschi Software">
+//   (c) 2015
+// </copyright>
+// <summary>
+//   Defines the RegistrationsController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Web.Mvc;
+
+namespace CourseBooking.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        // GET: Registrations
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
