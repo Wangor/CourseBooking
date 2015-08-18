5b66cedea1378d20a5eda16f00badfbe0002f096
Mattb <info@matt-b.ch>
2015-08-18 22:32:34 +0200

    mail

--- CourseBooking/Services/MailService.cs
+++ CourseBooking/Services/MailService.cs
@@ -65,6 +65,12 @@ namespace CourseBooking.Services
             }
         }
 
+        /// <summary>
+        /// The confirm registration direct.
+        /// </summary>
+        /// <param name="registrationId">
+        /// The registration id.
+        /// </param>
         public static void ConfirmRegistrationDirect(int registrationId)
         {
             var context = new CourseContext();
@@ -73,7 +79,7 @@ namespace CourseBooking.Services
             if (registration != null)
             {
 
-                SendGridMessage myMessage = new SendGridMessage();
+                var myMessage = new SendGridMessage();
                 myMessage.AddTo(registration.EMail);
                 myMessage.From = new MailAddress("gerry.gruetter@gmx.ch", "Fahrschule Grütter-Stooss");
                 myMessage.Subject = "Anmeldung erhalten";
@@ -96,14 +102,6 @@ namespace CourseBooking.Services
                     }
                 }
 
-                // Create credentials, specifying your user name and password.
-                var credentials = new NetworkCredential("7c64bd24-10db-495c-ad66-af83bb44681f@apphb.com",
-                    "7c64bd24-10db-495c-ad66-af83bb44681f@apphb.com");
-
-                // Create an Web transport for sending email, using credentials...
-                //var transportWeb = new Web(credentials);
-
-                // ...OR create a Web transport, using API Key (preferred)
                 var transportWeb = new Web("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");
 
                 // Send the email.
@@ -111,6 +109,12 @@ namespace CourseBooking.Services
             }
         }
 
+        /// <summary>
+        /// The confirm registration inform backoffice.
+        /// </summary>
+        /// <param name="registrationId">
+        /// The registration id.
+        /// </param>
         public static void ConfirmRegistrationInformBackoffice(int registrationId)
         {
             var context = new CourseContext();
@@ -119,20 +123,34 @@ namespace CourseBooking.Services
             if (registration != null)
             {
 
-                SendGridMessage myMessage = new SendGridMessage();
+                var myMessage = new SendGridMessage();
                 myMessage.AddTo("matt@matt-b.ch");
+                myMessage.AddTo("gerry.gruetter@gmx.ch");
                 myMessage.From = new MailAddress("gerry.gruetter@gmx.ch", "Fahrschule Grütter-Stooss");
                 myMessage.Subject = string.Format("Anmeldung erhalten von {0} {1}", registration.Name, registration.PreName);
 
                 if (registration.Courses.Count == 1)
                 {
+                    var sb = new StringBuilder();
+                    sb.AppendLine(string.Format("{0} {1}", registration.Name, registration.PreName));
+                    sb.AppendLine(string.Format("{0}", registration.AddressLine));
+                    sb.AppendLine(string.Format("{0} {1}", registration.Zip, registration.City));
+                    sb.AppendLine(string.Format("{0}", registration.Phone));
+
                     var course = registration.Courses.ElementAt(0);
-                    myMessage.Text = string.Format("Für den Kurs {0} am {1}", course.Name, course.StartDateTime);
+                    sb.AppendLine(string.Format("Für den Kurs {0} am {1}", course.Name, course.StartDateTime));
+                    
+                    myMessage.Text = sb.ToString();
                 }
 
                 if (registration.Courses.Count > 1)
                 {
                     var sb = new StringBuilder();
+                    sb.AppendLine(string.Format("{0} {1}", registration.Name, registration.PreName));
+                    sb.AppendLine(string.Format("{0}", registration.AddressLine));
+                    sb.AppendLine(string.Format("{0} {1}", registration.Zip, registration.City));
+                    sb.AppendLine(string.Format("{0}", registration.Phone));
+
                     sb.AppendLine("Für die Kurse:");
                     foreach (var course in registration.Courses)
                     {
@@ -142,14 +160,6 @@ namespace CourseBooking.Services
                     }
                 }
 
-                // Create credentials, specifying your user name and password.
-                var credentials = new NetworkCredential("7c64bd24-10db-495c-ad66-af83bb44681f@apphb.com",
-                    "7c64bd24-10db-495c-ad66-af83bb44681f@apphb.com");
-
-                // Create an Web transport for sending email, using credentials...
-                //var transportWeb = new Web(credentials);
-
-                // ...OR create a Web transport, using API Key (preferred)
                 var transportWeb = new Web("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");
 
                 // Send the email.
